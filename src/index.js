996657371c363d754795c711b7f05baa731e4d9f
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-05-20 07:49:59 -0700

    finished first two deliverables

--- src/index.js
+++ src/index.js
@@ -5,28 +5,60 @@
 
 const dogsUrl = "http://localhost:3000/dogs"
 
+
 document.addEventListener('DOMContentLoaded', () => {
+  const dogsTable = document.getElementsByTagName('table')[0]
+  // console.log(dogsTable)
+
+  const makeDogTr = (dogs) => {
+    dogs.forEach(dog => {
+      const dogTr = document.createElement('tr')
+      dogTr.innerHTML = `
+      <td>${dog.name}</td>
+      <td>${dog.breed}</td>
+      <td>${dog.sex}</td> 
+      <td>
+        <button>Edit</button>
+      </td>
+      `
+      dogsTable.appendChild(dogTr)
+    });
+  };
+  
+  const fetchDogsFromApi = (url) => {
+    fetch(dogsUrl)
+    .then(resp => resp.json())
+    .then(makeDogTr)
+  };
 
   fetchDogsFromApi(dogsUrl);
 
+
+  dogsTable.addEventListener('click', (e) => {
+    
+  });
+
+
+
 })
 
-const fetchDogsFromApi = (url) => {
-  fetch(url)
-  .then(resp => resp.json())
-  .then(makeDogTr)
-};
+// const fetchDogsFromApi = (url) => {
+//   fetch(url)
+//   .then(resp => resp.json())
+//   .then(makeDogTr)
+// };
 
-const makeDogTr = (dogs) => {
-  dogs.forEach(dog => {
-    const dogTr = document.createElement('tr')
-    dogTr.innerHTML = `
-    <td>${dog.name}</td>
-    <td>${dog.breed}</td>
-    <td>${dog.sex}</td> 
-    <td>
-      <button>Edit</button>
-    </td>
-    `
-  });
-};
+// const makeDogTr = (dogs) => {
+//   dogs.forEach(dog => {
+//     const dogTr = document.createElement('tr')
+//     dogTr.innerHTML = `
+//     <td>${dog.name}</td>
+//     <td>${dog.breed}</td>
+//     <td>${dog.sex}</td> 
+//     <td>
+//       <button>Edit</button>
+//     </td>
+//     `
+//     dogsTable.appendChild(dogTr)
+//   });
+// };
