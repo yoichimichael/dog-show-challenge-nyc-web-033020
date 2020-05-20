fb9b189010cf8b356a2f54c8e063f564962bf094
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-05-20 07:27:59 -0700

    starting on adding dogs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
+// on page load 
+  // fetch dogs
+  // make table row for each
+  // add table row to table
+
+const dogsUrl = "http://localhost:3000/dogs"
+
 document.addEventListener('DOMContentLoaded', () => {
 
+  fetchDogsFromApi(dogsUrl);
+
 })
+
+const fetchDogsFromApi = (url) => {
+  fetch(url)
+  .then(resp => resp.json())
+  .then(makeDogTr)
+};
+
+const makeDogTr = (dogs) => {
+  dogs.forEach(dog => {
+    const dogTr = document.createElement('tr')
+    dogTr.innerHTML = `
+    <td>${dog.name}</td>
+    <td>${dog.breed}</td>
+    <td>${dog.sex}</td> 
+    <td>
+      <button>Edit</button>
+    </td>
+    `
+  });
+};
